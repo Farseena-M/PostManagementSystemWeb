51bae9cb47ab4c771f59a8d80217131def46d7e4
Farseena-M <farseenamoyan19@gmail.com>
2024-11-23 08:54:53 +0530

    ..

--- Post/src/conponents/Signup.jsx
+++ Post/src/conponents/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post(`${url}/signup`, {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/signup`, {
                 username,
                 email,
                 password,
